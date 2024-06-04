746d7f81437c31b46efdddfe91978c3099e85078
Chris O'Hara <cohara87@gmail.com>
2024-06-04 12:29:45 +1000

    Wrap the Call message

---
+++ call.go
@@ -0,0 +1,97 @@
+package dispatch
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	sdkv1 "buf.build/gen/go/stealthrocket/dispatch-proto/protocolbuffers/go/dispatch/sdk/v1"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+// Call is a function call.
+type Call struct {
+	message *sdkv1.Call
+}
+
+// NewCall creates a Call.
+func NewCall(endpoint, function string, input proto.Message, opts ...CallOption) (Call, error) {
+	inputAny, err := anypb.New(input)
+	if err != nil {
+		return Call{}, fmt.Errorf("cannot serialize call input: %w", err)
+	}
+
+	call := Call{&sdkv1.Call{
+		Endpoint: endpoint,
+		Function: function,
+		Input:    inputAny,
+	}}
+	for _, opt := range opts {
+		opt(&call)
+	}
+	return call, nil
+}
+
+// CallOption configures a call.
+type CallOption func(*Call)
+
+// WithExpiration sets a function call expiration.
+func WithExpiration(expiration time.Duration) CallOption {
+	return CallOption(func(call *Call) { call.message.Expiration = durationpb.New(expiration) })
+}
+
+// WithCorrelationID sets a function call correlation ID.
+func WithCorrelationID(correlationID uint64) CallOption {
+	return CallOption(func(call *Call) { call.message.CorrelationId = correlationID })
+}
+
+// WithVersion sets a function call version.
+func WithVersion(version string) CallOption {
+	return CallOption(func(call *Call) { call.message.Version = version })
+}
+
+// Endpoint is the URL of the service where the function resides.
+func (c Call) Endpoint() string {
+	return c.message.GetEndpoint()
+}
+
+// Function is the name of the function to call.
+func (c Call) Function() string {
+	return c.message.GetFunction()
+}
+
+// Input is input to the function.
+func (c Call) Input() (proto.Message, error) {
+	input := c.message.GetInput()
+	if input == nil {
+		return nil, errors.New("no input")
+	}
+	return c.message.Input.UnmarshalNew()
+}
+
+// Expiration is the maximum time the function is allowed to run.
+func (c Call) Expiration() (time.Duration, bool) {
+	expiration := c.message.GetExpiration()
+	if expiration == nil {
+		return 0, false
+	}
+	return c.message.Expiration.AsDuration(), true
+}
+
+// Version of the application to select during execution.
+// The version is an optional field and not supported by all platforms.
+func (c Call) Version() string {
+	return c.message.GetVersion()
+}
+
+// CorrelationID is an opaque value that gets repeated in CallResult to
+// correlate asynchronous calls with their results.
+func (c Call) CorrelationID() uint64 {
+	return c.message.GetCorrelationId()
+}
+
+func (c Call) proto() *sdkv1.Call {
+	return c.message
+}
---
+++ call_test.go
@@ -0,0 +1,90 @@
+package dispatch
+
+import (
+	"testing"
+	"time"
+
+	sdkv1 "buf.build/gen/go/stealthrocket/dispatch-proto/protocolbuffers/go/dispatch/sdk/v1"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func TestCall(t *testing.T) {
+	t.Run("with no options", func(t *testing.T) {
+		call, err := NewCall("endpoint1", "function2", wrapperspb.Int32(11))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got := call.Endpoint(); got != "endpoint1" {
+			t.Errorf("unexpected call endpoint: %v", got)
+		}
+		if got := call.Function(); got != "function2" {
+			t.Errorf("unexpected call function: %v", got)
+		}
+		if got, err := call.Input(); err != nil || !proto.Equal(got, wrapperspb.Int32(11)) {
+			t.Errorf("unexpected call input: %v, %v", got, err)
+		}
+		if got := call.CorrelationID(); got != 0 {
+			t.Errorf("unexpected call correlation ID: %v", got)
+		}
+		if got, ok := call.Expiration(); ok {
+			t.Errorf("unexpected call expiration: %v, %v", got, ok)
+		}
+		if got := call.Version(); got != "" {
+			t.Errorf("unexpected call version: %v", got)
+		}
+
+		inputAny, _ := anypb.New(wrapperspb.Int32(11))
+		want := &sdkv1.Call{
+			Endpoint: "endpoint1",
+			Function: "function2",
+			Input:    inputAny,
+		}
+		if got := call.proto(); !proto.Equal(got, want) {
+			t.Errorf("unexpected call proto message: %#v", got)
+		}
+	})
+
+	t.Run("with options", func(t *testing.T) {
+		call, err := NewCall("endpoint1", "function2", wrapperspb.Int32(11),
+			WithCorrelationID(1234), WithExpiration(10*time.Second), WithVersion("xyzzy"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got := call.Endpoint(); got != "endpoint1" {
+			t.Errorf("unexpected call endpoint: %v", got)
+		}
+		if got := call.Function(); got != "function2" {
+			t.Errorf("unexpected call function: %v", got)
+		}
+		if got, err := call.Input(); err != nil || !proto.Equal(got, wrapperspb.Int32(11)) {
+			t.Errorf("unexpected call input: %v, %v", got, err)
+		}
+		if got := call.CorrelationID(); got != 1234 {
+			t.Errorf("unexpected call correlation ID: %v", got)
+		}
+		if got, ok := call.Expiration(); !ok || got != 10*time.Second {
+			t.Errorf("unexpected call expiration: %v, %v", got, ok)
+		}
+		if got := call.Version(); got != "xyzzy" {
+			t.Errorf("unexpected call version: %v", got)
+		}
+
+		inputAny, _ := anypb.New(wrapperspb.Int32(11))
+		want := &sdkv1.Call{
+			Endpoint:      "endpoint1",
+			Function:      "function2",
+			Input:         inputAny,
+			CorrelationId: 1234,
+			Expiration:    durationpb.New(10 * time.Second),
+			Version:       "xyzzy",
+		}
+		if got := call.proto(); !proto.Equal(got, want) {
+			t.Errorf("unexpected call proto message: %#v", got)
+		}
+	})
+}
