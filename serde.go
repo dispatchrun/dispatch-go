e5933647e7b489c7803beffb20c6f1df41bc3c3f
Chris O'Hara <cohara87@gmail.com>
2024-06-17 11:58:21 +1000

    Need to store type URL alongside serialized proto.Message

--- serde.go
+++ serde.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/dispatchrun/coroutine/types"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func init() {
@@ -21,12 +22,19 @@ func protoSerializer(s *types.Serializer, mp *proto.Message) error {
 		types.SerializeT(s, false)
 		return nil
 	}
-	b, err := proto.Marshal(m)
+
+	any, err := anypb.New(m)
+	if err != nil {
+		return fmt.Errorf("anypb.New: %w", err)
+	}
+	b, err := proto.Marshal(any)
 	if err != nil {
 		return fmt.Errorf("proto.Marshal: %w", err)
 	}
+
 	types.SerializeT(s, true)
 	types.SerializeT(s, b)
+
 	return nil
 }
 
@@ -41,9 +49,13 @@ func protoDeserializer(d *types.Deserializer, mp *proto.Message) error {
 	var b []byte
 	types.DeserializeTo(d, &b)
 
-	var m proto.Message
-	if err := proto.Unmarshal(b, m); err != nil {
+	var any anypb.Any
+	if err := proto.Unmarshal(b, &any); err != nil {
 		return fmt.Errorf("proto.Unmarshal: %w", err)
+	}
+	m, err := any.UnmarshalNew()
+	if err != nil {
+		return fmt.Errorf("anypb.UnmarshalNew: %w", err)
 	}
 	*mp = m
 
